188e00f6a914f618e32cfef3df05b64107b7fd44
Nenko Ivanov <nenko.ivanov@outlook.com>
2025-04-25 13:24:37 +0300

    initial commit: - window for the game - basic movement -player sprite

---
+++ src/main/GamePanel.java
@@ -0,0 +1,88 @@
+package main;
+import entity.Player;
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    //Screen settings
+    final int originalTileSize = 16; // 16x16 tile (default size for entities)
+    final int scale = 3;
+
+    public final int tileSize = originalTileSize * scale; //48x48 tile
+
+    //16 tiles horizontally and 12 vertically
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; //768 pixels
+    final int screenHeight = tileSize * maxScreenRow; //576 pixels
+
+    //FPS
+    int FPS = 60;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    Player player = new Player(this, keyH);
+
+    //set players default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        //game loop
+        long nanoSecond = 1000000000; //equivalent to 1s
+        double drawInterval = nanoSecond / FPS; //0.01666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while(gameThread != null) {
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= nanoSecond) {
+                System.out.println("FPS" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+        player.draw(g2);
+        g2.dispose();
+    }
+}
